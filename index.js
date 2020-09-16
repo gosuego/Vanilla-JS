a7e0a90622d15959bc6e30707baf033071f7ff1e
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-16 19:36:56 +0900

    console

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const calculator = {
   plus: function (a, b) {
     return a + b
   },
+  minus: function (a, b) {
+    return a - b
+  },
+  x: function (a, b) {
+    return a * b
+  },
 }
 
 const plus = calculator.plus(5, 5)
+const minus = calculator.minus(3, 5)
+const x = calculator.x(4, 5)
 
 console.log(plus)
+console.log(minus)
+console.log(x)
