21269c6bac3c3ac74477814de20d0483fd3cf42a
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-16 19:19:29 +0900

    calculator

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-alert("Im Working. Im JS. Im Beautiful. Im worth it")
-console.log("Im Working. Im JS. Im Beautiful. Im worth it")
+const calculator = {
+  plus: function (a, b) {
+    return a + b
+  },
+}
+
+const plus = calculator.plus(5, 5)
+
+console.log(plus)
