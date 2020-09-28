94044ab34f85f28becbe4e284788919ebfceb35a
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-28 14:52:05 +0900

    array

--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,6 @@
 // Array
+const mon = "monday"
 
-const makeArray = ["hi", "I", "am", "making", "a", "array", "1", "2", "3", "4"]
+const makeArray = ["hi", "I", "am", "making", "a", "array", "1", "2", "3", "4", mon]
 
 console.log(makeArray)
