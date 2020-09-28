47458817114a85bd4c5ae9d2f19ffb6c08229ea9
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-28 15:32:03 +0900

    object

--- practice/index.js
+++ practice/index.js
@@ -1,6 +1,21 @@
 // Array
-const mon = "monday"
+/*const mon = "monday"
 
 const makeArray = ["hi", "I", "am", "making", "a", "array", "1", "2", "3", "4", mon]
 
 console.log(makeArray)
+*/
+//Object
+
+const go = {
+  name: "gosue",
+  age: 24,
+  live: "songpa",
+  array: ["hi", "this", "is", "array"],
+
+  favoritFood: {
+    food: ["라면", "떡복이", "김밥"],
+  },
+}
+
+console.log(go)
