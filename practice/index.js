4cd28718e57ad7b4984d1a31d8d0f63405c6c6ed
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-29 09:06:59 +0900

    calculator.object

--- practice/index.js
+++ practice/index.js
@@ -5,3 +5,23 @@ function myName(name, age, live) {
 const sayHEllo = myName("GoSue", 24, "songpa")
 
 console.log(sayHEllo)
+
+const calculator = {
+  plus: function (a, b) {
+    return a + b
+  },
+  minus: function (c, d) {
+    return c - d
+  },
+  x: function (o, p) {
+    return o * p
+  },
+}
+
+const plus = calculator.plus(5, 5)
+const minus = calculator.minus(3, 5)
+const x = calculator.x(4, 5)
+
+console.log(plus)
+console.log(minus)
+console.log(x)
