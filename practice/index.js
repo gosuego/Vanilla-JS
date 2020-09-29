8cdb591ed3eebc6b337289cec6cece3c479fcdb7
gosuego <56798016+gosuego@users.noreply.github.com>
2020-09-29 09:47:14 +0900

    document

--- practice/index.js
+++ practice/index.js
@@ -1,27 +1,5 @@
-function myName(name, age, live) {
-  return `Hello ${name} my age is ${age} I live in ${live}`
-}
+const title = document.getElementById("title")
 
-const sayHEllo = myName("GoSue", 24, "songpa")
+console.log(title)
 
-console.log(sayHEllo)
-
-const calculator = {
-  plus: function (a, b) {
-    return a + b
-  },
-  minus: function (c, d) {
-    return c - d
-  },
-  x: function (o, p) {
-    return o * p
-  },
-}
-
-const plus = calculator.plus(5, 5)
-const minus = calculator.minus(3, 5)
-const x = calculator.x(4, 5)
-
-console.log(plus)
-console.log(minus)
-console.log(x)
+console.error("peep")
